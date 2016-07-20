782098cadd71fd659216e8d2c00ac1216d40eaab
dylanrees <dylanrees@protonmail.ch>
2016-07-20 02:41:38 -0400

    test

---
+++ README.md
@@ -0,0 +1,17 @@
+This repository contains files related to the "Science of the Slam" workshop for the Triple Helix project at RPI.
+Explain Triple Helix and also CDST.
+To fully utilize this repo you will also need an Arduino and to build the photogate equipment described on the CDST website (coming soon).
+
+
+==TO USE THE PROGRAM
+
+1.) Use the Arduino IDE to load the analogread.ino program in this repo onto your Arduino.
+
+2.) Set up the DIY Arduino photogate hardware described above.
+
+3.) Connect the Arduino to your computer with a USB port.
+
+4.) On the same computer, start up the Python program display.py, also from this repo.
+
+5.) Begin the demonstration!
+
---
+++ display.py
@@ -0,0 +1,59 @@
+# python program for Triple Helix "Science of the Slam" photogate workshop
+# by Dylan Rees
+
+import serial #PySerial library for interfacing with the Arduino
+import time
+import matplotlib #for plotting
+import numpy as np #for plotting
+import matplotlib.pyplot as plt #for plotting
+import matplotlib.patches as mpatches #used for drawing the legend on the graphs
+
+#starting a serial connection with the arduino
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+data = [] #will hold all arduino data points
+airtime = 0 #variable that stores the jump time
+jumpup = 0 #variable that stores the last time "jumping" became true
+jumpdown = 0 #variable that stores the last time "jumping" became false
+jumping = False #boolean variable that stores whether or not somebody is jumping
+timekeeper = 0 #timekeeping variable.
+jumpheight = 0 #variable to store calculated jump height
+
+#initialize plot and plot for the first time
+plt.ion()
+plt.plot(data)
+plt.axis([0, 20, 0, 5])
+plt.ylabel('Jump Plates')
+plt.legend(['Airtime: '+str(airtime)+'\n Jumping: '+str(jumping)+'\n Jump Height:'+str(jumpheight)+'m'])
+
+#plotting loop
+while True:
+	plt.plot(data)
+	print(arduino.readline())
+	try:
+		state = float(arduino.readline())/205 #division by 205 should convert arduino analog reading into units of volts
+		if state>3 and jumping == False:
+			jumping = True
+			jumpup = timekeeper
+		if state<=3 and jumping == True:
+			jumping = False
+			jumpdown = timekeeper
+			airtime = jumpdown - jumpup
+			jumpheight = 9.81*airtime*airtime/8 #use kinematics formulat to calculate jump height based on airtime
+		data.append(state)
+		print state
+		timekeeper = timekeeper + 0.1
+	except:
+		pass
+	# route to scroll the graph by discarding the oldest data point before redrawing
+	if len(data)>20:
+		data.pop(0)
+		plt.clf()
+		plt.axis([0, 20, 0, 5])
+		plt.ylabel('Jump Plates')
+		plt.plot(data)
+		plt.legend(['Airtime: '+str(airtime)+'\n Jumping: '+str(jumping)+'\n Jump Height:'+str(jumpheight)+'m'])
+	plt.draw()
+
+
+
